483b7dc780ff2b3173d5db27b81c27ba0f050019
Abel <abelper54@gmail.com>
2021-03-30 15:18:58 -0400

    Nuevos cambios

--- src/repairs.js
+++ src/repairs.js
@@ -9,28 +9,17 @@ async function cleanLinks(target) {
 async function fixLinks(target) {
     let pattern = /(?: ?\([^\)\s]+[ :]?(?:\[|(?:\\\[))+((?:rc:|https:|\.*)?\/\/?(?:[\w-_\.]+\/)*((?:[A-Za-z-_]+(?:\d+)?))(?:\/[0-9]+)?(?:\.\w+)?\/?)\/?(?:\]|\\\])*(?:\)|\])*[^\n \.])|(?:\[|(?:\\\[))+((?:rc:|https:|\.*)?\/\/?(?:[\w-_\.]+\/)*((?:[A-Za-z-_]+(?:\d+)?))(?:\/[0-9]+)?(?:\.\w+)?\/?)(?:\]|(?:\\\]))*/gm;
 
-    let fixed = cleanLinks(target);
-    fixed = fixed.replace(pattern, (match, p1, p2, p3, p4) => {
+    let fixed = await cleanLinks(target)
+    .then( (response) => response.replace(pattern, (match, p1, p2, p3, p4) => {
 
-        let path = '';
-        let slug = '';
+        let path = p1 ? p1 : p3;
+        let slug = p2 ? p2 : p4;
 
-        if (p1) {
-            path = p1;
-            slug = p2;
-        }
-
-        if (p3) {
-            path = p3;
-            slug = p4;
-        }
-
-        let tituloPartes = slug.split('-');
-        tituloPartes = tituloPartes.map((string) => string[0].toUpperCase() + string.slice(1));
+        let tituloPartes = slug.split('-').map((string) => string[0].toUpperCase() + string.slice(1));
         let titulo = tituloPartes.join(' ');
         let enlace = '[' + titulo + ']' + '(' + path + ')';
         return ` (ver: ${enlace})`;
-    });
+    }));
     return fixed;
 }
 async function fixAsterisk(target) {
@@ -47,50 +36,52 @@ async function fixQuotes(target) {
 
     let pattern = /"([^"]+)"|'([^"']+)'/gm;
 
-    let fixed = cleanQuotes(target);
-    fixed = fixed.replace(pattern, (match, p1, p2) => {
+    let fixed = cleanQuotes(target)
+    .then( response => response.replace(pattern, (match, p1, p2) => {
 
         if (p1) {
-            p1 = p1.replace(/'([^"']+)'/gm, (match, p) => {
+            let output = p1.replace(/'([^"']+)'/gm, (match, p) => {
                 return `‘${p}’`;
             });
             //console.log(`“${p1}”`);
-            return `“${p1}”`;
+            return `“${output}”`;
         }
 
         if (p2) {
             //console.log(`‘${p2}’`);
             return `‘${p2}’`;
         }
-    });
+    }));
 
     return fixed;
 }
 async function fixTitles(target) {
     let pattern = /(?:^\s+)?#+(.+)\s+/gm;
-    let fixed = target.replace(pattern, (match, p) => {
-        p = cleanEdges(p);
-        return `\n# ${p}\n\n`;
+    let fixed = await target.replace(pattern, (match, p) => {
+        
+        let output = cleanEdges(p)
+
+        return `\n\n# ${output}\n\n`;
     });
     return fixed;
 }
-async function cleanEdges(target) {
+function cleanEdges(target) {
     let pattern = /^(\s+)|(\s+)$/g;
     let fixed = target.replace(pattern, (match, p1, p2) => {
         if (p1)
             return ''
         else
-            return '\n\n'
+            return '\n'
     });
     return fixed;
 }
 async function refactorList(target) {
-    let pattern = /^(?: *\* *_* *([^_\*\n]*[^-]) *_*\s*-?\s+)(?!\*|#)(?:([^#\n]+)) *$/gm;
+    let pattern = /^(?: *\* *_* *([^_\*\n]*[^-])(?:[\s_-]+)+\s+)(?!\*|#)(?:([^#\n]+)) *$/gm;
     let fixed = target.replace(pattern, (match, p1, p2) => {
         if (p1 && p2)
-            return `\n# ${cleanEdges(p1)}\n\n${cleanEdges(p2)}\n`;
-
-        return p1 ? `\n* ${cleanEdges(p1)}\n\n` : `\n* ${cleanEdges(p2)}\n\n`;
+            return `\n# ${cleanEdges(p1)}\n\n${cleanEdges(p2)}\n`
+        else
+            return p1 ? `\n* ${cleanEdges(p1)}\n\n` : `\n* ${cleanEdges(p2)}\n\n`
     });
     return fixed;
 }
@@ -142,14 +133,12 @@ async function lint(target){
 
 export async function fixAll(target) {
 
-        fixed = await cleanOBS(target)
-
-        .then( response => cleanOBS(response) )
+        let fixed = await cleanOBS(target)
         .then( response => refactorList(response) )
-        .then( response => fixLinks(response) )
         .then( response => fixQuotes(response) )
         .then( response => fixAsterisk(response) )
         .then( response => fixTitles(response) )
+        .then( response => fixLinks(response) )
         .then( response => cleanEdges(response) )
 
 /*         fixed = await refactorList(fixed);
